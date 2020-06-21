ac14c26384eead7ac98b18a0f82b8cea16919df8
Damian Monogue <demonnic@gmail.com>
2020-06-21 17:59:19 -0400

    Small typo

--- js/map.js
+++ js/map.js
@@ -597,7 +597,7 @@ class MapRenderer {
         let show = false;
         for (let userDataKey in userData) {
             show = true;
-            containerList.append("<li>" + userDataKey + ":<br>&nbsp; &nbsp; &nbsp;" + userData[userDataKey] + "</lu>");
+            containerList.append("<li>" + userDataKey + ":<br>&nbsp; &nbsp; &nbsp;" + userData[userDataKey] + "</li>");
         }
         container.toggle(show);
     }
