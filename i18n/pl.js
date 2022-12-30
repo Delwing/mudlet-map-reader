a88aeeae5b2721430989a76788087d636d0bc325
Piotr Wilczynski <delwing@gmail.com>
2022-12-30 11:09:18 +0100

    fix local translations

---
+++ i18n/pl.js
@@ -0,0 +1,17 @@
+translations = typeof(translations) !== "undefined" ? translations : {};
+translations.pl = {
+    "north": "północ",
+    "south": "południe",
+    "east": "wschód",
+    "west": "zachód",
+    "northeast": "północny-wschód",
+    "northwest": "północny-zachód",
+    "southeast": "południowy-wschód",
+    "southwest": "południowy-zachód",
+    "up": "góra",
+    "down": "dół",
+    "copied": "Skopiowana do schowka",
+    "no-clipboard": "Twoja przeglądarka nie wspiera kopiowania do schowka",
+    "settings-saved": "Ustawienia zapisane",
+    "location-not-found" : "Nie znaleziono takiej lokacji"
+}
